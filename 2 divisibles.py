4a9b16498417883f7467db9fca3d6c4e2c9e68fe
NaachoTamola <51387762+NaachoTamola@users.noreply.github.com>
2025-02-11 18:35:28 -0500

    Add files via upload

---
+++ 2 divisibles.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+if a % b == 0:  # Verificamos si el resto es exactamente igual a 0
+    print("The number is divisible")
+else:
+    print("The number is not divisible")
